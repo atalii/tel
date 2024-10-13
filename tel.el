b431e05214fdd3fb5140b749cfe83bb3a4f5ec05
Tali Auster <me@tali.network>
2024-10-13 09:25:11 -0700

    feat: add tel-open-dated-file
    This is just another general component that simplifies a bit of my
    config.

--- tel.el
+++ tel.el
@@ -22,3 +22,16 @@
 	     (progn
 	       (setq ,buf-store (current-buffer))
 	       (,action))))))))
+
+;; Tag a given string with the current date.
+(defun tel--date-fname (name)
+  (let ((suffix (format-time-string "%m-%d")))
+    (concat name "-" suffix)))
+
+;; Given a list of file names, prompt the user to select one and
+;; create or open a file with that name and the current date. Open it
+;; for editing.
+(defun tel-open-dated-file (prompt names path-prefix path-suffix)
+  (let ((name (completing-read prompt names)))
+    (find-file
+     (concat path-prefix "/" (tel--date-fname name) path-suffix))))
